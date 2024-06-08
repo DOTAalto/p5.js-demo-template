e2be18c7ea8a5eb1f562070b67bdcceeb1fc4bdc
GGeru <56148582+GGeru@users.noreply.github.com>
2024-06-08 12:52:36 +0300

    added shader files and modified test3D

--- test3D.js
+++ test3D.js
@@ -1,9 +1,28 @@
-//empty file, not anymore
+let firstShader
+
+function preload() {
+    firstShader = loadShader('shader.vert', 'shader.frag')
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+
+}
+
 audioPlayer = document.querySelector('#song')
 
 
 function draw() {
+    shader(firstShader)
+    rect(0,0,width,height)
     const bpm = 138
     const demoTime = getTime() * bpm / 60
 
+    background('orange')
+
+    const mainCamera = createCamera()
+
+    orbitControl()
+
+    box()
 }
